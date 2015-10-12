28f82bec7229b0b5a6651bfba85b58c4e957b6ce
root <root@localhost.localdomain>
2015-10-12 17:40:49 +0200

    added woocommerce files

---
+++ wp-content/themes/relook_theme/home.php
@@ -0,0 +1,20 @@
+<?php get_template_part('templates/page', 'header'); ?>
+<section class="intro-section">
+  hello
+</section>
+<section class="row">
+  <div class="col-md-12">  
+    <h1>Second section</h1>
+  </div>
+</section>
+<section class="row">
+  <div class="col-md-12">  
+    <h1>Third section</h1>
+  </div>
+</section>
+<section clss="row">
+  <div class="col-md-12">  
+    <h1>Fourth section</h1>
+  </div>
+</section>
+<?php the_posts_navigation(); ?>
